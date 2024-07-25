bea9599f9ebf8a1c044ad56fac8c50ccbb96b73d
dfvfdvdff <sdfdfdfds@gmail.com>
2024-07-25 16:18:14 +0700

    lalala

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,4 @@
-let express = require('express')
+/*let express = require('express')
 let bodyParser = require('body-parser')
 const app = express()
 app.use(bodyParser.json())
@@ -25,7 +25,7 @@ app.post('/user/', function (req, res) {
    })
    // edit user
 app.put('/user/:userId', function (req, res) {
-    res.send('ทาํการแกไ้ขผใุ้ชง้าน: ' + req.params.userId + ' : ' +
+    res.send('ทำการแก้ไขผู้ใช้งาน : ' + req.params.userId + ' : ' +
     JSON.stringify(req.body))
    })
    // delete user
@@ -38,4 +38,25 @@ let port=8081
 
 app.listen(port,function(){
     console.log('server running on ' + port)
+})*/
+
+let express = require('express')
+let bodyParser = require('body-parser')
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+require('./routes')(app)
+app.get('/status', function (req, res ){
+ res.send('Hello nodejs server')
+})
+app.get('/hello/:person', function (req,res) {
+ console.log('hello - ' + req.params.person)
+ res.send('sey hello with ' + req.params.person)
+})
+app.post('/hello', function (req, res) {
+ res.send('OK you post - ' + req.body.name)
+})
+let port = 8081
+app.listen(port, function () {
+ console.log('server running on ' + port)
 })
