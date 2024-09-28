ee2f1d9c6d23f4a2c97bffa7b3e3bd23bfc31bd9
dfvfdvdff <sdfdfdfds@gmail.com>
2024-09-28 18:25:05 +0700

    add client end s7

---
+++ client/src/components/Users/Index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Get All Users</h1>
+    </div>
+</template>
+<script>
+export default {
+    created() {
+        this.$http.get('http://localhost:8081/users')
+            .then(function (response) {
+                console.log(response)
+            })
+    }
+}
+</script>
+<style scoped></style>
