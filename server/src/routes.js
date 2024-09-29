a9e582871f5dbf714b5066e97358a72b7789db96
dfvfdvdff <sdfdfdfds@gmail.com>
2024-09-30 00:11:01 +0700

    add package token , login test

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,24 +1,28 @@
 const UserController = require('./controllers/UserController')
+const UserAuthenController = require('./controllers/UserAuthenController')
 module.exports = (app) => {
- /* RESFUL Api for users management */
- // create user
- app.post('/user',
- UserController.create
- )
- // edit user, suspend, active
- app.put('/user/:userId',
-    UserController.put
-)
-// delete user
-app.delete('/user/:userId',
-UserController.remove
-)
-// get user by id
-app.get('/user/:userId',
-UserController.show
-)
-// get all user
-app.get('/users',
-UserController.index
-)
+    /* RESFUL Api for users management */
+    // create user
+    app.post('/user',
+        UserController.create
+    )
+    // edit user, suspend, active
+    app.put('/user/:userId',
+        UserController.put
+    )
+    // delete user
+    app.delete('/user/:userId',
+        UserController.remove
+    )
+    // get user by id
+    app.get('/user/:userId',
+        UserController.show
+    )
+    // get all user
+    app.get('/users',
+        UserController.index
+    )
+    app.post('/login', 
+        UserAuthenController.login
+        )
 }
