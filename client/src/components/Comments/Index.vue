6da49479aefe9a2e627323abb886e505853620f5
dfvfdvdff <sdfdfdfds@gmail.com>
2024-10-10 16:47:54 +0700

    add blogs,comment route back font

--- client/src/components/Comments/Index.vue
+++ client/src/components/Comments/Index.vue
@@ -1,10 +1,55 @@
 <template>
     <div>
-        <h1>Comment Blog </h1>
+        <h2>Get all comments</h2>
+        <p><button v-on:click="logout">Logout</button></p>
+        <h4>จํานวน comment {{ comments.length }}</h4>
+        <div v-for="comment in comments" v-bind:key="comment.id">
+            <p>id: {{ comment.id }}</p>
+            <p>blog id: {{ comment.blogId }}</p>
+            <p>comment: {{ comment.comment }}</p>
+            <p>
+                <button v-on:click="navigateTo('/comment/' + comment.id)">ดู
+                    comment</button>
+                <button v-on:click="deleteComment(comment)">ลบข้อมูล</button>
+            </p>
+            <hr>
+        </div>
     </div>
 </template>
 <script>
+import CommentsService from '@/services/CommentsService'
 export default {
+    data() {
+        return {
+            comments: []
+        }
+    },
+    async created() {
+        this.comments = (await CommentsService.index()).data
+    },
+    methods: {
+        logout() {
+            this.$store.dispatch('setToken', null)
+            this.$store.dispatch('setComment', null)
+            this.$router.push({
+                name: 'login'
+            })
+        },
+        navigateTo(route) {
+            this.$router.push(route)
+        },
+        async deleteComment(comment) {
+            try {
+                await CommentsService.delete(comment)
+                this.refreshData()
+            } catch (err) {
+                console.log(err)
+            }
+        },
+        async refreshData() {
+            this.comments = (await CommentsService.index()).data
+        }
+    }
 }
 </script>
 <style scoped></style>
